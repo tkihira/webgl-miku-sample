ccc2addef05e407e145b98f55d270ace0e9c8c06
Takuo KIHIRA <tkihira@tombo.io>
2014-11-17 23:56:03 -0800

    add comments

--- miku.js
+++ miku.js
@@ -44,16 +44,19 @@
 			console.log(gl.getShaderInfoLog(fs));
 			return;
 		}
+		// リンクしたプログラムの使用を指示
 		gl.useProgram(prog);
 		loadBuffer();
 		drawFrame();
 	};
-	var vbuf;
-	var nbuf;
+	var vbuf; // 頂点座標バッファ
+	var nbuf; // 法線ベクトルバッファ
 	var loadBuffer = function() {
+		// 頂点座標に関し、バッファを生成してデータを指定
 		vbuf = gl.createBuffer();
 		gl.bindBuffer(gl.ARRAY_BUFFER, vbuf);
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-0.5, -0.5, 0, 0.5, -0.5, 0, 0.5, 0.5, 0]), gl.STATIC_DRAW);
+		// 法線ベクトルに関しても同上。配列長は頂点数3×軸3=9個
 		nbuf = gl.createBuffer();
 		gl.bindBuffer(gl.ARRAY_BUFFER, nbuf);
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0, 0, 1, 0, 0, 1, 0, 0, 1]), gl.STATIC_DRAW);
@@ -61,28 +64,35 @@
 	var frame = 0;
 	var drawFrame = function() {
 		frame++;
+		// frustum行列の生成
 		var proj_mat = mat4.create();
 		mat4.frustum(proj_mat, -1, 1, -1, 1, 3, 10);
+		// 移動回転行列の生成
 		var mv_mat = mat4.create();
 		mat4.translate(mv_mat, mv_mat, [0, 0, -6]);
-		mat4.rotate(mv_mat, mv_mat, frame * 0.1, [0, 1, 0]);
+		mat4.rotate(mv_mat, mv_mat, frame * 0.01, [0, 1, 0]); // 軸[0, 1, 0]で回転
+		// uniformでShaderに送信。4fvはfloat4つの配列（vector）という意味
 		gl.uniformMatrix4fv(gl.getUniformLocation(prog, "projectionMatrix"), false, proj_mat);
 		gl.uniformMatrix4fv(gl.getUniformLocation(prog, "modelviewMatrix"), false, mv_mat);
+		// Canvasの内容をクリア（色は RGB(0, 0, 0, 1): 黒）
 		gl.clearColor(0, 0, 0, 1);
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 		gl.enable(gl.DEPTH_TEST);
 
+		// attributeのindex(position)を取得
 		var vpos = gl.getAttribLocation(prog, "vertex");
 		var npos = gl.getAttribLocation(prog, "normal");
 
-		gl.bindBuffer(gl.ARRAY_BUFFER, vbuf);
-		gl.vertexAttribPointer(vpos, 3, gl.FLOAT, false, 0, 0);
+		// 取得したattribute positionにバッファを送信
+		gl.bindBuffer(gl.ARRAY_BUFFER, vbuf); // 頂点座標
+		gl.vertexAttribPointer(vpos, 3, gl.FLOAT, false, 0, 0); // float型を1頂点につき3つ、と指定
 		gl.enableVertexAttribArray(vpos);
 
-		gl.bindBuffer(gl.ARRAY_BUFFER, nbuf);
-		gl.vertexAttribPointer(npos, 3, gl.FLOAT, true, 0, 0);
+		gl.bindBuffer(gl.ARRAY_BUFFER, nbuf); // 法線ベクトル
+		gl.vertexAttribPointer(npos, 3, gl.FLOAT, true, 0, 0); // float型を1頂点につき3つ、と指定
 		gl.enableVertexAttribArray(npos);
 
+		// 今まで設定した内容でWebGLに送信
 		gl.drawArrays(gl.TRIANGLES, 0, 3);
 		setTimeout(drawFrame, 16);
 	};
